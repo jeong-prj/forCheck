f38fb1b465ca28afc8912971200bd7096f327959
jeong-prj <63942953+jeong-prj@users.noreply.github.com>
2022-09-05 20:32:23 +0900

    Add files via upload

--- include/find_positions_class.hpp
+++ include/find_positions_class.hpp
@@ -8,6 +8,7 @@
 #include "nav_msgs/OccupancyGrid.h"
 #include "map_msgs/OccupancyGridUpdate.h"
 #include "nav_msgs/MapMetaData.h"
+#include "nav_msgs/Odometry.h"
 #include <visualization_msgs/Marker.h>
 
 //#include <actionlib/client/simple_action_client.h>
@@ -58,24 +59,29 @@ public:
   
   //void setMarker(visualization_msgs::Marker& m_points, visualization_msgs::Marker& line_strip){
   void setMarker(){
-    m_points.header.frame_id = line_strip.header.frame_id= "map";
-    m_points.header.stamp = line_strip.header.stamp = ros::Time(0);
-    m_points.ns = line_strip.ns = "way_points";
+    m_points.header.frame_id = line_strip.header.frame_id = t2_line_strip.header.frame_id= "map";
+    m_points.header.stamp = line_strip.header.stamp = t2_line_strip.header.stamp = ros::Time(0);
+    m_points.ns = line_strip.ns = t2_line_strip.ns = "way_points";
     m_points.id=1000;
     m_points.type=visualization_msgs::Marker::POINTS;
     line_strip.type=visualization_msgs::Marker::LINE_STRIP;
+    t2_line_strip.type=visualization_msgs::Marker::LINE_STRIP;  
     
-    m_points.action = line_strip.action =visualization_msgs::Marker::ADD;
-    m_points.pose.orientation.w = line_strip.pose.orientation.w =1.0;
+    m_points.action = line_strip.action  =t2_line_strip.action  =visualization_msgs::Marker::ADD;
+    m_points.pose.orientation.w = line_strip.pose.orientation.w = t2_line_strip.pose.orientation.w =1.0;
     
     m_points.scale.x = 0.1;
     m_points.scale.y = 0.1;
     
     line_strip.scale.x = 0.03;
+    t2_line_strip.scale.x = 0.03;
 
     m_points.color.b = 1.0f;
     m_points.color.a = 1.0;
     
+    
+    t2_line_strip.color.b=1.0f;
+    t2_line_strip.color.a=1.0;
     line_strip.color.b=1.0f;
     line_strip.color.a=1.0;
     
@@ -99,29 +105,51 @@ public:
     
     m_path_points.lifetime = ros::Duration(); 
     
-    //tsp solver result 2m
-    tsp_points.header.frame_id = "map";
-    tsp_points.header.stamp = ros::Time(0);
-    tsp_points.ns = "way_apath";
-    tsp_points.id=3000;
-    tsp_points.type=visualization_msgs::Marker::POINTS;
+    //First tsp solver result 
+    tsp2_points.header.frame_id = "map";
+    tsp2_points.header.stamp = ros::Time(0);
+    tsp2_points.ns = "way_apath";
+    tsp2_points.id=3000;
+    tsp2_points.type=visualization_msgs::Marker::POINTS;
     
-    tsp_points.action = visualization_msgs::Marker::ADD;
-    tsp_points.pose.orientation.w = 1.0;
+    tsp2_points.action = visualization_msgs::Marker::ADD;
+    tsp2_points.pose.orientation.w = 1.0;
     
-    tsp_points.scale.x = 0.15;
-    tsp_points.scale.y = 0.15;
+    tsp2_points.scale.x = 0.15;
+    tsp2_points.scale.y = 0.15;
 
-    tsp_points.color.r = 1.0f;
-    tsp_points.color.a = 1.0;
+    tsp2_points.color.r = 1.0f;
+    tsp2_points.color.a = 1.0;
     
-    tsp_points.lifetime = ros::Duration(); 
+    tsp2_points.lifetime = ros::Duration(); 
+    
+    //tsp solver result 2m
+    tsp1_points.header.frame_id = t1_line_strip.header.frame_id ="map";
+    tsp1_points.header.stamp = t1_line_strip.header.stamp = ros::Time(0);
+    tsp1_points.ns = t1_line_strip.ns = "way_apath";
+    tsp1_points.id = t1_line_strip.id =4000;
+    tsp1_points.type=visualization_msgs::Marker::POINTS;
+    t1_line_strip.type=visualization_msgs::Marker::LINE_STRIP;  
+    
+    tsp1_points.action = t1_line_strip.action = visualization_msgs::Marker::ADD;
+    tsp1_points.pose.orientation.w = t1_line_strip.pose.orientation.w = 1.0;
+    
+    tsp1_points.scale.x = 0.045;
+    tsp1_points.scale.y = 0.045;
+
+    t1_line_strip.scale.x = 0.03;
+    t1_line_strip.scale.y = 0.03;
+    
+    tsp1_points.color.b = t1_line_strip.color.b = 1.0f;
+    tsp1_points.color.a = t1_line_strip.color.a = 1.0;
+    
+    tsp1_points.lifetime = t1_line_strip.lifetime = ros::Duration(); 
     
     //added point
     add_points.header.frame_id = "map";
     add_points.header.stamp = ros::Time(0);
     add_points.ns = "way_apath";
-    add_points.id=3000;
+    add_points.id=5000;
     add_points.type=visualization_msgs::Marker::POINTS;
     
     add_points.action = visualization_msgs::Marker::ADD;
@@ -178,17 +206,31 @@ public:
   void findWaypointsDistance(int *tour);
   void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg);
   void globalCostmapCallBack(const nav_msgs::OccupancyGrid::ConstPtr& msg ) ;
+  void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg);
   
   //int eraseInvalidByDist();
   //int bfs(int node_size, vector<int>* graph, vector<int>& sequence);
   void makeMapForPath();
-  
   double calculatePath(double ax, double ay, double bx, double by, int mode);
-  int FindFirstNode();
+  double takeATime(ros::WallTime start, ros::WallTime end){
+    return (end - start).toNSec()*1e-6;
+  }
+  
+  int findFirstNode();
   void checkRealPath();
   int m_mapAvailable = 0;
+  int m_sposeAvailable = 0;
   
   vector<array<double,2>> result_waypoints;
+  
+  ros::WallTime totalStartTime, totalEndTime;
+  ros::WallTime tsp1StartTime, tsp1EndTime;
+  ros::WallTime tsp2StartTime, tsp2EndTime;
+  ros::WallTime scanningStartTime, scanningEndTime;
+  
+  double totalTime, tsp1Time, tsp2Time, scanningTime;
+  
+  
   
 protected:
   //                     up ri  do  le
@@ -197,14 +239,15 @@ protected:
   
   ros::NodeHandle m_nh;
   
-  ros::Subscriber m_gridmapsub, m_globalCostmapSub;
-  ros::Publisher m_wayPointsPub, m_pathPub, m_tspPointsPub, m_addPointsPub;
+  ros::Subscriber m_gridmapsub, m_globalCostmapSub, m_startPoseSub;
+  ros::Publisher m_wayPointsPub, m_pathPub, m_tsp2PointsPub, m_addPointsPub, m_tsp1PointsPub;
   
   TSP::TSPSolver m_TSPSolver;
   int map_width;
   int map_height;
   double map_resolution;
   array<double, 2> gridmap_origin_pose;
+  array<double, 2> start_pose;
   
   vector<signed char> origin_map;
   vector<signed char> map_flags;
@@ -213,8 +256,8 @@ protected:
   vector<array<double,2>> waypointsWorld;
   //ej_marker
   visualization_msgs::Marker add_points;
-  visualization_msgs::Marker tsp_points;
-  visualization_msgs::Marker m_points, line_strip;
+  visualization_msgs::Marker tsp1_points, tsp2_points;
+  visualization_msgs::Marker m_points, line_strip, t1_line_strip, t2_line_strip;
   visualization_msgs::Marker m_path_points;
   vector<array<int,2>> middles;
   vector<array<int,2>> waypoints;
